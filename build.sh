7f915354dddef174a70f700305969b31ff54f897
Ildar Bikmamatov <ildar@bayrell.org>
2022-05-15 16:42:36 +0000

    arm64 version

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ SCRIPT_PATH=`dirname $SCRIPT`
 BASE_PATH=`dirname $SCRIPT_PATH`
 
 RETVAL=0
+IMAGE="ubuntu_code_server"
 VERSION=3.11.0
-SUBVERSION=8
+SUBVERSION=9
 TAG=`date '+%Y%m%d_%H%M%S'`
 
 case "$1" in
@@ -14,28 +15,80 @@ case "$1" in
 	download)
 		mkdir -p downloads
 		if [ ! -f downloads/code-server-3.11.0-linux-amd64.tar.gz ]; then
-			wget https://github.com/cdr/code-server/releases/download/v3.11.0/code-server-3.11.0-linux-amd64.tar.gz -O "downloads/code-server-3.11.0-linux-amd64.tar.gz"
+			wget https://github.com/coder/code-server/releases/download/v3.11.0/code-server-3.11.0-linux-amd64.tar.gz -O "downloads/code-server-3.11.0-linux-amd64.tar.gz"
+		fi
+		if [ ! -f downloads/code-server-3.11.0-linux-arm64v8.tar.gz ]; then
+			wget https://github.com/coder/code-server/releases/download/v3.11.0/code-server-3.11.0-linux-arm64.tar.gz -O "downloads/code-server-3.11.0-linux-arm64v8.tar.gz"
 		fi
 		if [ ! -f downloads/go1.16.6.linux-amd64.tar.gz ]; then
 			wget https://golang.org/dl/go1.16.6.linux-amd64.tar.gz -O "downloads/go1.16.6.linux-amd64.tar.gz"
 		fi
+		if [ ! -f downloads/go1.16.6.linux-arm64v8.tar.gz ]; then
+			wget https://golang.org/dl/go1.16.6.linux-arm64.tar.gz -O "downloads/go1.16.6.linux-arm64v8.tar.gz"
+		fi
 	;;
 	
-	test)
-		docker build ./ -t bayrell/ubuntu_code_server:$VERSION-$SUBVERSION-$TAG --file stages/Dockerfile
+	test-amd64)
+		docker build ./ -t bayrell/$IMAGE:$VERSION-$SUBVERSION-$TAG-amd64 \
+			--file stages/Dockerfile --build-arg ARCH=amd64
+	;;
+	
+	test-arm64v8)
+		docker build ./ -t bayrell/$IMAGE:$VERSION-$SUBVERSION-$TAG-arm64v8 \
+			--file stages/Dockerfile --build-arg ARCH=arm64v8
+	;;
+	
+	stage0-amd64)
+		docker build ./ -t bayrell/$IMAGE:stage0-$TAG-amd64 \
+			--file stages/Dockerfile0 --build-arg ARCH=amd64
+	;;
+	
+	stage0-arm64v8)
+		docker build ./ -t bayrell/$IMAGE:stage0-$TAG-arm64v8 \
+			--file stages/Dockerfile0 --build-arg ARCH=arm64v8
 	;;
 	
 	amd64)
-		docker build ./ -t bayrell/ubuntu_code_server:$VERSION-$SUBVERSION-amd64 \
-			--file stages/Dockerfile --build-arg ARCH=amd64/
+		docker build ./ -t bayrell/$IMAGE:$VERSION-$SUBVERSION-amd64 \
+			--file stages/Dockerfile --build-arg ARCH=amd64
 	;;
 	
-	stage0)
-		docker build ./ -t bayrell/ubuntu_code_server:stage0 --file stages/Dockerfile0
+	arm64v8)
+		docker build ./ -t bayrell/$IMAGE:$VERSION-$SUBVERSION-arm64v8 \
+			--file stages/Dockerfile --build-arg ARCH=arm64v8
+	;;
+	
+	manifest)
+		rm -rf ~/.docker/manifests/docker.io_bayrell_$IMAGE-*
+		
+		docker tag bayrell/$IMAGE:$VERSION-$SUBVERSION-amd64 bayrell/$IMAGE:$VERSION-amd64
+		docker tag bayrell/$IMAGE:$VERSION-$SUBVERSION-arm64v8 bayrell/$IMAGE:$VERSION-arm64v8
+		
+		docker push bayrell/$IMAGE:$VERSION-$SUBVERSION-amd64
+		docker push bayrell/$IMAGE:$VERSION-$SUBVERSION-arm64v8
+		
+		docker push bayrell/$IMAGE:$VERSION-amd64
+		docker push bayrell/$IMAGE:$VERSION-arm64v8
+		
+		docker manifest create bayrell/$IMAGE:$VERSION-$SUBVERSION \
+			--amend bayrell/$IMAGE:$VERSION-$SUBVERSION-amd64 \
+			--amend bayrell/$IMAGE:$VERSION-$SUBVERSION-arm64v8
+		docker manifest push bayrell/$IMAGE:$VERSION-$SUBVERSION
+		
+		docker manifest create bayrell/$IMAGE:$VERSION \
+			--amend bayrell/$IMAGE:$VERSION-amd64 \
+			--amend bayrell/$IMAGE:$VERSION-arm64v8
+		docker manifest push bayrell/$IMAGE:$VERSION
+	;;
+	
+	all)
+		$0 amd64
+		$0 arm64v8
+		$0 manifest
 	;;
 	
 	*)
-		echo "Usage: $0 {download|amd64|test|stage0}"
+		echo "Usage: $0 {download|all|amd64|arm64v8|test-amd64|test-arm64v8|stage0-amd64|stage0-arm64v8}"
 		RETVAL=1
 
 esac
