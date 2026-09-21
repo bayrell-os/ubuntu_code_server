d1fc4388706b1dbd283d755659b392e1ac8881b1
Ildar Bikmamatov <ildar@bayrell.org>
2026-09-21 21:05:28 +0500

    remove files

--- files/root/run.d/10-home.sh
+++ files/root/run.d/10-home.sh
@@ -2,22 +2,27 @@ if [ ! -d /data/home ]; then
 	mkdir -p /data/home
 	chown -R ubuntu:ubuntu /data/home
 fi
+
 if [ ! -f /data/home/.profile ]; then
 	cp -f /root/files/.profile /data/home/.profile
 	chown ubuntu:ubuntu /data/home/.profile
 fi
+
 if [ ! -f /data/home/.bashrc ]; then
 	cp -f /root/files/.bashrc /data/home/.bashrc
 	chown ubuntu:ubuntu /data/home/.bashrc
 fi
+
 if [ ! -d /data/home/node_modules/.bin ]; then
 	mkdir -p /data/home/node_modules/.bin
 	chown -R ubuntu:ubuntu /data/home/node_modules
 fi
+
 if [ ! -d /data/home/bin ]; then
 	mkdir -p /data/home/bin
 	chown -R ubuntu:ubuntu /data/home/bin
 fi
+
 if [ ! -z $TZ ]; then
 	ln -snf /usr/share/zoneinfo/$TZ /etc/localtime; \
 	echo "$TZ" > /etc/timezone; \
--- files/root/run.d/20-code-settings.sh
+++ files/root/run.d/20-code-settings.sh
@@ -10,11 +10,6 @@ if [ ! -d /data/home/code/extensions ]; then
 	chown ubuntu:ubuntu /data/home/code/extensions
 fi
 
-if [ ! -d /data/home/code/User ]; then
-	mkdir -p /data/home/code/User
-	chown ubuntu:ubuntu /data/home/code/User
-fi
-
 if [ "$CODE_SERVER_ENABLE_ADMIN" = "1" ]; then
 	sed -i 's|127.0.0.1:8000|0.0.0.0:8000|g' /etc/supervisor.d/code-server.ini
 	rm -f /etc/supervisor.d/nginx.ini
